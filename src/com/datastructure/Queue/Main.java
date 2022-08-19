d18b92dfb542865d7845ffb9eca0525742dc6edb
LexieLiu19 <xuliu0925@gmail.com>
2022-08-19 18:49:07 -0400

    add test case to queue

--- src/com/datastructure/Queue/Main.java
+++ src/com/datastructure/Queue/Main.java
@@ -6,12 +6,21 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
+        // Queue is an interface which can not be instantiated
+        //ArrayDeque is the implement class.
         Queue<Integer> queue = new ArrayDeque<>();
         queue.add(10);
         queue.add(20);
         queue.add(30);
+        System.out.println("Queue:");
         System.out.println(queue);
+
+        var front = queue.remove();
+        System.out.println("Queue after remove()");
+        System.out.println(queue);
+
         reverse(queue);
+        System.out.println("Reversed queue: ");
         System.out.println(queue);
     }
 
